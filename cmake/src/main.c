481e53817cf5771fcf4d90227b913fcabed1180b
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2024-01-10 16:12:44 +0800

    Finalize cmake

--- cmake/src/main.c
+++ cmake/src/main.c
@@ -2,6 +2,7 @@
 #include <pthread.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <unistd.h>
 // #include <unistd.h>
 
 // #include "progress.h"
@@ -19,9 +20,8 @@ int log4c_level = LOG4C_ALL;
 
 bool pick(const int i, Chopstick *c) {
   assert(NULL != c);
-  LOG_DEBUG("Try to get mutex [%d]", i);
+  LOG_DEBUG("[%d] Try to get mutex [%d]", i, c->index);
   pthread_mutex_lock(c->mutex);
-  // sleep(1);
   if (c->usedBy != -1) {
     // this chopstick is taken
     LOG_DEBUG("Chopstick [%d] is taken by [%d]", c->index, c->usedBy);
@@ -29,7 +29,7 @@ bool pick(const int i, Chopstick *c) {
     return false;
   }
   // this chopstick is available
-  LOG_DEBUG("Chopstick [%d] is available", i);
+  LOG_DEBUG("Chopstick [%d] is available", c->index);
   c->usedBy = i;
   LOG_DEBUG("[%d] take chopstick [%d]", c->usedBy, c->index);
   pthread_mutex_unlock(c->mutex);
@@ -38,6 +38,7 @@ bool pick(const int i, Chopstick *c) {
 
 void put(Chopstick* c) {
   assert(NULL != c);
+  LOG_DEBUG("[%d] try to put chopstick [%d] back", c->usedBy, c->index);
   pthread_mutex_lock(c->mutex);
   LOG_DEBUG("[%d] put chopstick [%d] back", c->usedBy, c->index);
   c->usedBy = -1;
@@ -46,7 +47,24 @@ void put(Chopstick* c) {
 
 void* runtime(void* input) {
   assert(NULL != input);
-  // Person* p = (Person*) input; // single person data
+  Person* p = (Person*) input;
+  while(true) {
+    if (pick(p->index, p->left) == false) {
+      // unable to pick left, repeat if unable to pick
+      continue;
+    }
+
+    if (pick(p->index, p->right) == false) {
+      // unable to pick right, release left as well
+      put(p->left);
+      continue;
+    }
+    LOG_INFO("Philosopher [%d] is eating", p->index);
+    sleep(rand() % 5);
+    LOG_INFO("Philosopher [%d] eat completed", p->index);
+    put(p->left);
+    put(p->right);
+  }
   pthread_exit(0);
 }
 
@@ -64,6 +82,7 @@ int main() {
 
   pthread_t threads[NUM_THREADS];
   for (int i = 0; i < NUMBER; ++i) {
+    LOG_INFO("Thread [%d] start", ps[i]->index);
     pthread_create(&threads[i], NULL, runtime, ps[i]);
   }
 
